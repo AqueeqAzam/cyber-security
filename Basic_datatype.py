0557b3e3dec527f486b1fbe8b9717d7454b3b820
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2023-02-03 13:23:01 +0530

    concept of basic data type

---
+++ Basic_datatype.py
@@ -0,0 +1,22 @@
+# I covering some basic of python how this works in cyber security
+# integers
+httpPort = 80
+print(httpPort)
+
+# floating point
+encrypt = 7.3/3
+print(encrypt)
+
+# string
+route = "https://github.com/AqueeqAzam/cyber-security"
+print(route)
+
+# boolean
+portOpen = True
+portClose = False
+portOpen = 3000
+portClose = 3001
+
+if(3000 < 3001):
+              print("working")
+else:("Not working")

1597f8f7810470ee97487980f9230a3248b79fec
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2023-02-03 15:21:18 +0530

    Update Basic_datatype.py

--- Basic_datatype.py
+++ Basic_datatype.py
@@ -5,18 +5,21 @@ port = 3000
 print(type(port))
 
 httpPort = 80
-print(httpPort)
+print("The port is running at", httpPort)
 
 # floating point
 encrypt = 7.3/3
-print(encrypt)
+print("This data is encrypted", encrypt)
 
 # string
 banner = "FreeFloat FTP Server"
 print(type(banner))
 
+memory_address = "nh9w7ey4fjw9"
+print("The data is placed at", memory_address)
+
 route = "https://github.com/AqueeqAzam/cyber-security"
-print(route)
+print("we can learn hacking at", route)
 
 # boolean
 portOpen = True
@@ -28,5 +31,5 @@ portOpen = 3000
 portClose = 3001
 
 if(3000 < 3001):
-              print("working")
+              print("My task is working, we can attack")
 else:("Not working")

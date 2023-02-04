91625c3b82e1469663ebb291a93ce0c6689588a0
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2023-02-04 13:35:04 +0530

    concept of decision making

---
+++ branching.py
@@ -0,0 +1,30 @@
+# Here I explore some intersting decesion making code
+pList = [21, 80, 404, 3000]
+if pList[0] == 80:
+    print("We are accessing exact port number")
+else:
+    print("Unknown service")
+    
+ # enter user name
+userName = input("Enter user name :")
+if userName == "21" and "Tyson":
+    print("We can attact")
+else:
+    print("processing fail")
+    
+# set a strong password
+psw = input("Enter password name :")
+if psw == "ty":
+    print("This password is strong ")
+else:
+    print("weak password")
+    
+# find a service
+pList = [21, 22, 25, 80]
+if pList[0] == 21:
+    print("FTP	Service")
+elif pList[0] == 22:
+    print("SSH	Service")
+else:
+    print("Unknown	Service")
+    
